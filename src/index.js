ec9e43c710646c4c67272e21d379eae90c1ba4a5
bManrique17 <bmanrique17@gmail.com>
2019-09-23 00:48:11 -0600

    Becoming millonarios :)

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const expbhs = require('express-handlebars');
+const path = require('path');
+
+
+const app = express();
+
+app.set('views',path.join(__dirname, 'views'));
+app.engine('.hbs', expbhs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'),'layouts'),
+  parialsDir: path.join(app.get('views'),'partials'),
+  extname: '.hbs'
+}));
+
+
+app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(require('./routes/index'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, ()=>{
+  console.log('Server on port ',3000);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+const router = Router();
+const stripe = require('stripe')('sk_test_V6QhvoUYa4JYCOxrq0H2uBT400ACBLmL5M');
+
+router.get('/',(req,res) => {
+  res.render('index');
+});
+
+router.post('/checkout', async (req,res) => {
+  console.log(req.body);
+  const customer = await stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  });
+  const charge = await stripe.charges.create({
+    amount: '1700',
+    currency: 'usd',
+    customer: customer.id,
+    description: 'GTA San Andreas'
+  });
+  console.log(charge.id);
+  res.render('download');  
+})
+
+module.exports = router;
