f438af32aa19b841610cd20cfd802d0ef442e3fb
bManrique17 <bmanrique17@gmail.com>
2019-09-23 00:54:35 -0600

    Becoming millonarios x2 :)

--- src/index.js
+++ src/index.js
@@ -24,5 +24,5 @@ app.use(require('./routes/index'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, ()=>{
-  console.log('Server on port ',3000);
+  console.log('Server on port ',3001);
 });
