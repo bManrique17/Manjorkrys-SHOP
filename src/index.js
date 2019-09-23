4a7721e37667f128f1e5efb62176dc2432097e42
bManrique17 <bmanrique17@gmail.com>
2019-09-23 01:05:49 -0600

    Becoming millonarios x4 :)

--- src/index.js
+++ src/index.js
@@ -23,6 +23,6 @@ app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3001, ()=>{
-  console.log('Server on port ',3001);
+app.listen(8080, ()=>{
+  console.log('Server on port ',8080);
 });
