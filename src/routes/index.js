5d47cfa2ac9ec32e862769eb6cfd1d7a0bf56986
bManrique17 <bmanrique17@gmail.com>
2019-09-23 01:21:38 -0600

    Becoming millonarios x5 :)

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,7 +19,24 @@ router.post('/checkout', async (req,res) => {
     description: 'GTA San Andreas'
   });
   console.log(charge.id);
-  res.render('download');  
-})
+  res.render('download');
+});
+
+router.post('/piscina', async (req,res) => {
+  console.log(req.body);
+  const customer = await stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  });
+  const charge = await stripe.charges.create({
+    amount: '5000',
+    currency: 'usd',
+    customer: customer.id,
+    description: 'Piscina'
+  });
+  console.log(charge.id);
+  res.render('download');
+});
+
 
 module.exports = router;
